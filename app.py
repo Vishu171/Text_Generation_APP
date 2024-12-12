f00b1a841ddef0b3e44ef7dbc4f9c8e5e12bd9cd
Vishu171 <100827321+Vishu171@users.noreply.github.com>
2024-12-12 15:42:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
+from langchain.chat_models import AzureChatOpenAI
 import openai
 
-openai.api_key = "Azure_Openai_Key"
-openai.api_type = 'azure'
-openai.api_version = "2023-05-15"
-openai.api_base = "https://ai-upskilling.openai.azure.com/"
-deployment_name = "gpt-4o-batch3"
 
-# add your completion code
-prompt = "Complete the following: Once upon a time there was a"
-messages = [{"role": "user", "content": prompt}]
+# # Retrieve Azure OpenAI specific configuration from environment variables
+# OPENAI_API_KEY = os.getenv("AZURE_OPENAI_API_KEY")
+# OPENAI_API_TYPE = os.getenv("AZURE_OPENAI_API_TYPE")
+# OPENAI_API_BASE = os.getenv("AZURE_OPENAI_API_BASE")
+# OPENAI_API_VERSION = os.getenv("AZURE_OPENAI_API_VERSION")
 
-# make completion
-completion = openai.chat.completions.create(model='text-embedding-batch3', messages=messages)
+# Set the OpenAI library configuration using the retrieved environment variables
+openai.api_type = AZURE_OPENAI_API_KEY
+openai.api_base = AZURE_OPENAI_API_BASE
+openai.api_version = AZURE_OPENAI_API_VERSION
+openai.api_key = AZURE_OPENAI_API_KEY
 
-# print response
-print(completion.choices[0].message.content)
+# Initialize an instance of AzureChatOpenAI using the specified settings
+chat_llm = AzureChatOpenAI(
+    openai_api_version=OPENAI_API_VERSION,
+    openai_api_key=OPENAI_API_KEY,
+    openai_api_base=OPENAI_API_BASE,
+    openai_api_type=OPENAI_API_TYPE,
+    deployment_name="testing"  
+)
+
+# Print the response from AzureChatOpenAI for the same question
+print("AzureOpenAI ChatLLM Response: ", chat_llm.predict("what is the weather in mumbai today?"))
