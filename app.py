0b1eda6e30be7ab23d6e134290f44e55b20c85da
Vishu171 <100827321+Vishu171@users.noreply.github.com>
2024-12-12 18:48:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import openai
 import streamlit as st
 # Set the OpenAI library configuration using the retrieved environment variables
 
-st.header("AI-Powered Text Generatorion App")
+st.header("AI-Powered Text Generation App")
 
 # Initialize an instance of AzureChatOpenAI using the specified settings
 # chat_llm = AzureChatOpenAI(
